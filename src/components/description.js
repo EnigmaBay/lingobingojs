65a7074decc83b20bd3ec9ad041ac483687f1a35
Ryan Schafer <schaferyan@gmail.com>
2022-09-01 17:13:39 -0700

    added basic text formatting

--- src/components/description.js
+++ src/components/description.js
@@ -3,9 +3,12 @@ import React from 'react';
 function Description() {
   return (
     <div className="Description">
-      <h1>Step up your webinar</h1>
-      <p>LingoBingo creates an interactive experience for participants during webinars.
-        Participants select words from their LingoBingo board as they hear them, keeping their minds actively engaged with the speaker.</p>
+      <h1>Welcome to LingoBingo.js</h1>
+      <p>Your experince of Webinars is about to change forever.  </p>
+      <h2>Step up your webinar game</h2>
+      <p>LingoBingo creates an interactive experience for participants during webinars.</p>
+      <h2>How to Play</h2>
+      <p>Participants select words from their LingoBingo board as they hear them, keeping their minds actively engaged with the speaker.</p>
     </div>
   );
 }
