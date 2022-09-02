6c072e4c8d0751b0c2b61b773469326751c57a74
Ryan Schafer <schaferyan@gmail.com>
2022-09-01 17:04:24 -0700

    Created simple description component

---
+++ src/components/description.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Description() {
+  return (
+    <div className="Description">
+      <h1>Step up your webinar</h1>
+      <p>LingoBingo creates an interactive experience for participants during webinars.
+        Participants select words from their LingoBingo board as they hear them, keeping their minds actively engaged with the speaker.</p>
+    </div>
+  );
+}
+
+export default Description;
