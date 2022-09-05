62a011202e34a63068fcb96359c665a08386a5d3
Ryan Schafer <schaferyan@gmail.com>
2022-09-05 15:33:01 -0700

    removed Logo component and containing div from Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Logo from './Logo.js';
 import About from './About.js';
 import Description from './Description.js';
 import NavbarMain from './NavbarMain.js';
@@ -12,9 +11,6 @@ export default function Home() {
   return (
     <div>
       <header>
-        <div className='logo-container'>
-          <Logo />
-        </div>
         <Container fluid>
           <NavbarMain/>
         </Container>
