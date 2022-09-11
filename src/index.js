e8b8f44c7907957300a65c48146d1899fd21ec39
Ryan Schafer <schaferyan@gmail.com>
2022-09-11 15:26:33 -0700

    added react router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './root.css';
-import App from './App';
+// import App from './App';
+import Home from './Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './root.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}>
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
