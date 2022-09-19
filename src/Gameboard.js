8419e38dcaadbc13d0c45ee05a11713f64ac8f83
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-09-19 16:24:56 -0700

    gameboard sends props to wordtile.

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -3,35 +3,36 @@ import Words from './itu-wordlist.json';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import WordTile from './WordTile.js';
 import './tempstyle.css';
 
 export default function Gameboard() {
   const rowOneWords = Words.map((item, idx) => {
     if (idx >= 0 && idx <= 4) {
-      return <Col key={idx} xs={2} className='word-tile'><div className='center-tile-content'>{item.word}</div></Col>;
+      return <Col key={idx} xs={2} className='word-tile'><div className='center-tile-content'><WordTile word={item.word} /></div></Col>;
     }
   });
   const rowTwoWords = Words.map((item, idx) => {
     if (idx >= 5 && idx <= 9) {
-      return <Col key={idx} xs={2} className='word-tile'><div className='center-tile-content'>{item.word}</div></Col>;
+      return <Col key={idx} xs={2} className='word-tile'><div className='center-tile-content'><WordTile word={item.word} /></div></Col>;
     }
   });
   const rowThreeWords = Words.map((item, idx) => {
     if (idx === 12) {
-      return <Col key={idx} xs={2} className='word-tile'><div className='center-tile-content'>FREE</div></Col>;
+      return <Col key={idx} xs={2} className='word-tile'><div className='center-tile-content'><WordTile word={'FREE'} /></div></Col>;
     }
     if (idx >= 10 && idx <= 14) {
-      return <Col key={idx} xs={2} className='word-tile'><div className='center-tile-content'>{item.word}</div></Col>;
+      return <Col key={idx} xs={2} className='word-tile'><div className='center-tile-content'><WordTile word={item.word} /></div></Col>;
     }
   });
   const rowFourWords = Words.map((item, idx) => {
     if (idx >= 15 && idx <= 19) {
-      return <Col key={idx} xs={2} className='word-tile'><div className='center-tile-content'>{item.word}</div></Col>;
+      return <Col key={idx} xs={2} className='word-tile'><div className='center-tile-content'><WordTile word={item.word} /></div></Col>;
     }
   });
   const rowFiveWords = Words.map((item, idx) => {
     if (idx >= 20 && idx <= 24) {
-      return <Col key={idx} xs={2} className='word-tile'><div className='center-tile-content'>{item.word}</div></Col>;
+      return <Col key={idx} xs={2} className='word-tile'><div className='center-tile-content'><WordTile word={item.word} /></div></Col>;
     }
   });
 
