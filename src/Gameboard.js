f0aa9ffed32c57eaf7c2789573eb3128ef67fa06
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-09-23 11:32:54 -0700

    refactored to leverage funcLib functions

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,41 +1,43 @@
 import React from 'react';
-import Words from './itu-wordlist.json';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import DauberLayer from './DauberLayer';
+import randomGen from './funcLib/RandomGen.js';
+import wordImporter from './funcLib/WordImporter.js';
+import wordProcessor from './funcLib/WordProcessor.js';
 import './tempstyle.css';
 
 export default class Gameboard extends React.Component {
+  rowBuilder(randWords) {
+    let result = [];
+    for (let row = 0; row < 5; row++) {
+      result.push(<Row key={row} className='mx-0'>{this.tileBuilder(row, randWords)}</Row>);
+    }
+    return result;
+  }
+
+  tileBuilder(row, randWords) {
+    console.log('Gameboard itemBuilder(row) accessing param randWords: ' + randWords);
+    let result = [];
+    const incrementor = row * 5;
+    for (let col=0; col < 5; col++) {
+      console.log('Gameboard itemBuilder(row) col: ' + col);
+      const idx = col + incrementor;
+      console.log('Gameboard itemBuilder(row) idx: ' + idx);
+      const currentWord = randWords[idx];
+      console.log('Gameboard itemBuilder(row) currentWord: ' + currentWord);
+      result.push(<Col key={idx} className='word-tile'><DauberLayer styleClass='daubered' word={currentWord}/></Col>);
+    }
+    return result;
+  }
+
   render() {
-    const rowOneWords = Words.map((item, idx) => {
-      if (idx >= 0 && idx <= 4) {
-        return <Col key={idx} xs={2} className='word-tile'><DauberLayer styleclass={'daubered'} word={item.word} /></Col>;
-      }
-    });
-    const rowTwoWords = Words.map((item, idx) => {
-      if (idx >= 5 && idx <= 9) {
-        return <Col key={idx} xs={2} className='word-tile'><DauberLayer styleclass={'plain'} word={item.word} /></Col>;
-      }
-    });
-    const rowThreeWords = Words.map((item, idx) => {
-      if (idx === 12) {
-        return <Col key={idx} xs={2} className='word-tile'><DauberLayer styleclass={'daubered'} word={'FREE'} /></Col>;
-      }
-      if (idx >= 10 && idx <= 14) {
-        return <Col key={idx} xs={2} className='word-tile'><DauberLayer styleclass={'plain'} word={item.word} /></Col>;
-      }
-    });
-    const rowFourWords = Words.map((item, idx) => {
-      if (idx >= 15 && idx <= 19) {
-        return <Col key={idx} xs={2} className='word-tile'><DauberLayer styleclass={'daubered'} word={item.word} /></Col>;
-      }
-    });
-    const rowFiveWords = Words.map((item, idx) => {
-      if (idx >= 20 && idx <= 24) {
-        return <Col key={idx} xs={2} className='word-tile'><DauberLayer styleclass={'plain'} word={item.word} /></Col>;
-      }
-    });
+    const randInts = randomGen(24);
+    let words = wordImporter();
+    const randWords = wordProcessor(words, randInts);
+    console.log('Gameboard randWords: ' + randWords);
+    const rows = this.rowBuilder(randWords);
 
     return (
       <Container fluid className='px-0' >
@@ -44,31 +46,7 @@ export default class Gameboard extends React.Component {
           <Col xs={'auto'}>Lets Play Bingo!</Col>
           <Col></Col>
         </Row>
-        <Row className='mx-0'>
-          <Col xs={1}></Col>
-          {rowOneWords}
-          <Col xs={1}></Col>
-        </Row>
-        <Row className='mx-0'>
-          <Col xs={1}></Col>
-          {rowTwoWords}
-          <Col xs={1}></Col>
-        </Row>
-        <Row className='mx-0'>
-          <Col xs={1}></Col>
-          {rowThreeWords}
-          <Col xs={1}></Col>
-        </Row>
-        <Row className='mx-0'>
-          <Col xs={1}></Col>
-          {rowFourWords}
-          <Col xs={1}></Col>
-        </Row>
-        <Row className='mx-0'>
-          <Col xs={1}></Col>
-          {rowFiveWords}
-          <Col xs={1}></Col>
-        </Row>
+        {rows}
       </Container>
     );
   }
