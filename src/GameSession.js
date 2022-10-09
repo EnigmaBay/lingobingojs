2d91fc2b1cae2d7bb61dcfcf2de540d937765a78
Ryan Schafer <schaferyan@gmail.com>
2022-10-08 19:52:03 -0700

    centered the button

--- src/GameSession.js
+++ src/GameSession.js
@@ -4,15 +4,24 @@ import randomGen from './funcLib/RandomGen.js';
 import wordProcessor from './funcLib/WordProcessor.js';
 import wordImporter from './funcLib/WordImporter.js';
 import PlayAgainButton from './PlayAgainButton.js';
+import { Col, Container, Row } from 'react-bootstrap';
 
 export default function GameSession() {
   const randInts = randomGen(24);
   let words = wordImporter();
   const randWords = wordProcessor(words, randInts);
   return (
-    <div>
-      <Gameboard randwords={randWords} />
-      <PlayAgainButton className='center-button' disabled={false}/>
-    </div>
+    <Container fluid>
+      <Row>
+        <Col>
+          <Gameboard randwords={randWords} />
+        </Col>
+      </Row>
+      <Row>
+        <Col className='d-flex justify-content-center'> {/* Center the button */}
+          <PlayAgainButton disabled={false}/>
+        </Col>
+      </Row>
+    </Container>
   );
 }
