58c8073ebcac7c92150a786d18d9be6aed4c16d5
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-10-10 10:34:45 -0700

    removed comments and console logs

--- src/funcLib/CalcBingoX.js
+++ src/funcLib/CalcBingoX.js
@@ -5,30 +5,20 @@ function CalcBingoX(tiles) {
   let topLeftBottomRightCount = 0;
   let topRightBottomLeftCount = 0;
 
-  // console.log('tiles is now an array? ' + Array.isArray(tiles));
-  // console.log('how long is it? ' + tiles.length);
-
   tiles.map((tile, idx) => {
-    // console.log('idx: ' + idx);
-    // console.log('tile: ' + tile);
 
     if (tile === true) {
-      // console.log('tile ' + idx + ' contains true');
 
       if (topLeftBottomRightTiles.includes(idx)) {
-        // console.log('idx ' + idx + ' is in tlbr.');
         topLeftBottomRightCount++;
       }
+
       if (topRightBottomLeftTiles.includes(idx)) {
-        // console.log('idx ' + idx + ' is in trbl.');
         topRightBottomLeftCount++;
       }
     }
     return;
   });
-
-  // console.log('topLeftBottomRightCOunt: ' + topLeftBottomRightCount);
-  // console.log('topRightBottomLeftCount: ' + topRightBottomLeftCount);
 
   const result = topLeftBottomRightCount === 5 || topRightBottomLeftCount === 5;
 
