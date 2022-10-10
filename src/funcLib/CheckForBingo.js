48d8ef3453f9ae2bdee7388d25d0d6988b97948c
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-10-10 10:36:00 -0700

    refactored checkforbingo Updated to check on 4 or more moves. Collapsed local variables

--- src/funcLib/CheckForBingo.js
+++ src/funcLib/CheckForBingo.js
@@ -3,24 +3,17 @@ const calcBingoH = require('./CalcBingoH');
 const calcBingoV = require('./CalcBingoV');
 
 function CalculateBingo(tiles,moves) {
-  let xResult = false;
-  let hResult = false;
-  let vResult = false;
   let result = false;
 
-  if (moves > 5) {
-    xResult = calcBingoX(tiles);
-    hResult = calcBingoH(tiles);
-    vResult = calcBingoV(tiles);
+  if (moves >= 4) {
+    const xResult = calcBingoX(tiles);
+    const hResult = calcBingoH(tiles);
+    const vResult = calcBingoV(tiles);
     result = (
       xResult === true ||
       hResult === true ||
       vResult === true
     );
-  }
-
-  if (moves === 5) {
-    result = calcBingoX(tiles);
   }
 
   return (
