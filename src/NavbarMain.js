09162d528341bc1223e0d7dd9ab8bef3c5499d69
Ryan Schafer <schaferyan@gmail.com>
2022-10-10 16:46:51 -0700

    replaced bootstrap Nav.Link elements with ReactRouter NavLink elements, which is neccessary to enable client-side routing

--- src/NavbarMain.js
+++ src/NavbarMain.js
@@ -2,20 +2,21 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-bootstrap';
 
 export default function NavbarMain() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="/">LingoBingo</Navbar.Brand>
+        <Navbar.Brand to="../">LingoBingo</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About Us</Nav.Link>
-            <Nav.Link href="/signup">Signup</Nav.Link>
-            <Nav.Link href="/login">Login</Nav.Link>
-            <Nav.Link href="/play">Play LingoBingo</Nav.Link>
+            <NavLink href="../">Home</NavLink>
+            <NavLink href="../about">About Us</NavLink>
+            <NavLink href="../signup">Signup</NavLink>
+            <NavLink href="../login">Login</NavLink>
+            <NavLink href="../play">Play LingoBingo</NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
