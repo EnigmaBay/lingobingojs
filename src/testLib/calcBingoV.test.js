107ae656f4ccae892c0a9fe86332c5b4b929436d
Jon Rumsey <nojronatron@users.noreply.github.com>
2022-10-13 12:45:41 -0700

    Update src/testLib/calcBingoV.test.js
    Local test passes.
    
    Co-authored-by: Ryan Schafer <30915304+schaferyan@users.noreply.github.com>

--- src/testLib/calcBingoV.test.js
+++ src/testLib/calcBingoV.test.js
@@ -3,7 +3,6 @@ const testArrays = require('./testData/verticalBingoPatterns.json');
 const patterns = [];
 
 testArrays.forEach((instance) => {
-  const tileList = [];
   patterns.push(
     {
       pattern: instance.pattern,
