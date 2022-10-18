90e3a77d94814f126252943b6b5e6497b42a091c
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-10-17 17:34:45 -0700

    updated main navbar Added candidate logo to navbar brand Inserted div to improve nav bar item layout.

--- src/NavbarMain.js
+++ src/NavbarMain.js
@@ -3,22 +3,32 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { NavLink } from 'react-router-dom';
+import logo67x40 from './lingo-bingo-logo-bravo-67-40.png';
 
 export default function NavbarMain() {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
+    <Navbar bg="dark" variant="dark" expand="sm">
       <Container>
-        <Navbar.Brand to="../">LingoBingo</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <NavLink className="nav-link main-nav-link" to="../">Home</NavLink>
-            <NavLink className="nav-link main-nav-link" to="../about">About Us</NavLink>
-            <NavLink className="nav-link main-nav-link" to="../signup">Signup</NavLink>
-            <NavLink className="nav-link main-nav-link" to="../login">Login</NavLink>
-            <NavLink className="nav-link main-nav-link" to="../play">Play LingoBingo</NavLink>
-          </Nav>
-        </Navbar.Collapse>
+        <Navbar.Brand to="../">
+          <img
+            src={logo67x40}
+            alt="Partial bingo board with words lingo bingo emblazoned over the top."
+            width={'67px'}
+            height={'40px'}
+          />
+        </Navbar.Brand>
+        <div>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <NavLink className="nav-link main-nav-link" to="../">Home</NavLink>
+              <NavLink className="nav-link main-nav-link" to="../about">About Us</NavLink>
+              <NavLink className="nav-link main-nav-link" to="../signup">Signup</NavLink>
+              <NavLink className="nav-link main-nav-link" to="../login">Login</NavLink>
+              <NavLink className="nav-link main-nav-link" to="../play">Play LingoBingo</NavLink>
+            </Nav>
+          </Navbar.Collapse>
+        </div>
       </Container>
     </Navbar>
   );
