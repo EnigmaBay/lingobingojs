751f713ae611ee3345f874f50f968f1c6bdd62de
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-10-17 17:38:03 -0700

    tweaked bootstrap cols and text to tidy up the page.

--- src/Description.js
+++ src/Description.js
@@ -5,62 +5,58 @@ import Col from 'react-bootstrap/Col';
 
 export default function Description() {
   return (
-    <Container
-      style={{
-        border: 'solid 1px #9F4A04',
-      }}
-    >
+    <Container fluid='md'>
       <Row>
-        <Col></Col>
-        <Col sm={8}>
-          <h1>Welcome to LingoBingo.js</h1>
+        <Col md='auto'></Col>
+        <Col className='md-8'>
+          <h1 className='text-center'>Welcome to LingoBingo.js</h1>
         </Col>
-        <Col></Col>
+        <Col md='auto'></Col>
       </Row>
       <Row>
-        <Col></Col>
-        <Col sm={8}>
-          <p>Your experince of Webinars is about to change forever.</p>
+        <Col md='auto'></Col>
+        <Col className='md-8'>
+          <p className='text-center'>Your experience of Webinars is about to change forever.</p>
         </Col>
-        <Col></Col>
+        <Col md='auto'></Col>
       </Row>
       <Row>
-        <Col></Col>
-        <Col sm={8}>
-          <h2>Make Your Webinars Interactive</h2>
+        <Col md='auto'></Col>
+        <Col className='md-8'>
+          <h2 className='text-center'>Make Your Webinars Interactive</h2>
         </Col>
-        <Col></Col>
+        <Col md='auto'></Col>
       </Row>
       <Row>
-        <Col></Col>
-        <Col sm={8}>
-          <p>
+        <Col md='auto'></Col>
+        <Col className='md-8'>
+          <p className='text-center'>
             LingoBingo creates a more engaging experience for participants
             during webinars. Participants select words from their LingoBingo
             board as they hear them, keeping their minds actively engaged with
             the speaker.
           </p>
         </Col>
-        <Col></Col>
+        <Col md='auto'></Col>
       </Row>
       <Row>
-        <Col></Col>
-        <Col sm={8}>
-          <h2>How to Play</h2>
+        <Col md='auto'></Col>
+        <Col className='md-8'>
+          <h2 className='text-center'>How to Play</h2>
         </Col>
-        <Col></Col>
+        <Col md='auto'></Col>
       </Row>
       <Row>
-        <Col></Col>
-        <Col sm={8}>
-          <p>
+        <Col md='auto'></Col>
+        <Col className='md-8'>
+          <p className='text-center'>
             If you are attending a webinar and have been invited to join a
             LingoBingo game, use the link provided by the host to join a game.
             LingoBingo will create a bingo board for you based on the vocabulary
             chosen by your host.
           </p>
         </Col>
-        <Col></Col>
+        <Col md='auto'></Col>
       </Row>
     </Container>
   );
