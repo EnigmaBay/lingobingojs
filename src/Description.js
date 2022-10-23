20833d9c178875e25b884b7012ce00ab5acd65fa
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-10-23 14:08:49 -0700

    Description content and theming Added more content to use for experimenting with color schemes and styles. Applied a theme for experimentation.

--- src/Description.js
+++ src/Description.js
@@ -5,36 +5,38 @@ import Col from 'react-bootstrap/Col';
 
 export default function Description() {
   return (
-    <Container fluid='md' className='main-output-borders'>
+    <Container fluid='md' className='main-output-borders light-scheme'>
       <Row>
         <Col md='auto'></Col>
         <Col className='md-8'>
-          <h1 className='text-center'>Welcome to LingoBingo.js</h1>
+          <h1>Welcome to LingoBingo.js</h1>
         </Col>
         <Col md='auto'></Col>
       </Row>
       <Row>
         <Col md='auto'></Col>
         <Col className='md-8'>
-          <p className='text-center'>Your experience of Webinars is about to change forever.</p>
+          <h2>Make Your Webinars Interactive</h2>
         </Col>
         <Col md='auto'></Col>
       </Row>
       <Row>
         <Col md='auto'></Col>
         <Col className='md-8'>
-          <h2 className='text-center'>Make Your Webinars Interactive</h2>
-        </Col>
-        <Col md='auto'></Col>
-      </Row>
-      <Row>
-        <Col md='auto'></Col>
-        <Col className='md-8'>
-          <p className='text-center'>
+          <p>
             LingoBingo creates a more engaging experience for participants
             during webinars. Participants select words from their LingoBingo
-            board as they hear them, keeping their minds actively engaged with
-            the speaker.
+            board as they hear them, keeping their minds actively engaged with the speaker.
+          </p>
+          <p>
+            LingoBingo creates a more engaging experience for participants
+            during webinars. Participants select words from their LingoBingo
+            board as they hear them, keeping their minds actively engaged with the speaker.
+          </p>
+          <p>
+            LingoBingo creates a more engaging experience for participants
+            during webinars. Participants select words from their LingoBingo
+            board as they hear them, keeping their minds actively engaged with the speaker.
           </p>
         </Col>
         <Col md='auto'></Col>
@@ -42,18 +44,26 @@ export default function Description() {
       <Row>
         <Col md='auto'></Col>
         <Col className='md-8'>
-          <h2 className='text-center'>How to Play</h2>
+          <h2>How to Play</h2>
         </Col>
         <Col md='auto'></Col>
       </Row>
-      <Row>
+      <Row className='m-1'>
         <Col md='auto'></Col>
-        <Col className='md-8'>
-          <p className='text-center'>
+        <Col className='md-8 px-4 py-1 rounded-4 dark-scheme'>
+          <p>
             If you are attending a webinar and have been invited to join a
             LingoBingo game, use the link provided by the host to join a game.
-            LingoBingo will create a bingo board for you based on the vocabulary
-            chosen by your host.
+          </p>
+          <p>
+            LingoBingo will create a bingo board for you based on the vocabulary chosen by your host.
+          </p>
+          <p>
+            If you are attending a webinar and have been invited to join a
+            LingoBingo game, use the link provided by the host to join a game.
+          </p>
+          <p>
+            LingoBingo will create a bingo board for you based on the vocabulary chosen by your host.
           </p>
         </Col>
         <Col md='auto'></Col>
