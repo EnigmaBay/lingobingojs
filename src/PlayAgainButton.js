c650627424ac43caf18a3a7cb8653959c3d390c1
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-10-23 17:49:09 -0700

    playagainbutton: changed to div, applied theme

--- src/PlayAgainButton.js
+++ src/PlayAgainButton.js
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import Button from 'react-bootstrap/Button';
 
-
-export default function PlayAgainButton(props){
-  return <Button onClick={props.handleClick}>
-    {props.isBingoed ? 'Play Again' : 'Restart Game'}
-  </Button>;
+export default function PlayAgainButton(props) {
+  return (
+    <div
+      className='p-1 m-2 rounded-4 lets-play light-theme-text'
+      onClick={props.handleClick}>
+      {props.isBingoed ? 'Play Again' : 'Restart Game'}
+    </div>
+  );
 }
 
 PlayAgainButton.propTypes = {
