bc982dcd39d59bf63ef20bd5b40265ae504bfc5a
Jon Rumsey <Jon.Rumsey@gmail.com>
2023-03-15 22:37:06 -0700

    edited double quotes to single for netlify.

--- src/Components/Description.js
+++ src/Components/Description.js
@@ -11,27 +11,27 @@ export default function Description() {
   return (
     <Container
       fluid
-      className="main-output-borders themed-background page"
+      className='main-output-borders themed-background page'
       data-theme={theme}
     >
-      <Row className="p-2">
-        <Col md="auto"></Col>
-        <Col className="md-8"></Col>
-        <Col md="auto"></Col>
+      <Row className='p-2'>
+        <Col md='auto'></Col>
+        <Col className='md-8'></Col>
+        <Col md='auto'></Col>
       </Row>
-      <Row className="p-2">
-        <Col md="auto"></Col>
-        <Col className="md-8">
-          <h2 className="themed-header" data-theme={theme}>
+      <Row className='p-2'>
+        <Col md='auto'></Col>
+        <Col className='md-8'>
+          <h2 className='themed-header' data-theme={theme}>
             Make Your Webinars Interactive
           </h2>
         </Col>
-        <Col md="auto"></Col>
+        <Col md='auto'></Col>
       </Row>
-      <Row className="p-2">
-        <Col md="auto"></Col>
-        <Col className="md-8">
-          <div className="rounded-4 themed-text" data-theme={theme}>
+      <Row className='p-2'>
+        <Col md='auto'></Col>
+        <Col className='md-8'>
+          <div className='rounded-4 themed-text' data-theme={theme}>
             LingoBingo creates a more engaging experience for participants
             during webinars. Participants select words from their LingoBingo
             board as they hear or see them during a presentation. This keeps
@@ -39,46 +39,46 @@ export default function Description() {
             presentation content.
           </div>
         </Col>
-        <Col md="auto"></Col>
+        <Col md='auto'></Col>
       </Row>
-      <Row className="p-2">
-        <Col md="auto"></Col>
-        <Col className="md-8">
-          <h2 className="themed-header" data-theme={theme}>
+      <Row className='p-2'>
+        <Col md='auto'></Col>
+        <Col className='md-8'>
+          <h2 className='themed-header' data-theme={theme}>
             How to Play
           </h2>
         </Col>
-        <Col md="auto"></Col>
+        <Col md='auto'></Col>
       </Row>
-      <Row className="p-2 mb-3">
-        <Col md="auto"></Col>
-        <Col className="md-8">
-          <div className="rounded-4 themed-text" data-theme={theme}>
+      <Row className='p-2 mb-3'>
+        <Col md='auto'></Col>
+        <Col className='md-8'>
+          <div className='rounded-4 themed-text' data-theme={theme}>
             If you are attending a webinar and have been invited to join a
             LingoBingo game, use the link provided by the host to join the game.
             LingoBingo will create a bingo board for you based on the vocabulary
             chosen by your host.
           </div>
         </Col>
-        <Col md="auto"></Col>
+        <Col md='auto'></Col>
       </Row>
-      <Row className="p-2 mb-3">
-        <div className="video-wrapper">
+      <Row className='p-2 mb-3'>
+        <div className='video-wrapper'>
           <iframe
-            className="demo-video rounded-4"
-            src="https://www.youtube.com/embed/P1yVieUmLPo?controls=0&autoplay=1&mute=1&modestbranding=1&loop=1&playlist=P1yVieUmLPo"
-            title="Video showing how to play LingoBingoJS"
-            allow="autoplay; encrypted-media;"
-            width="560"
-            height="315"
+            className='demo-video rounded-4'
+            src='https://www.youtube.com/embed/P1yVieUmLPo?controls=0&autoplay=1&mute=1&modestbranding=1&loop=1&playlist=P1yVieUmLPo'
+            title='Video showing how to play LingoBingoJS'
+            allow='autoplay; encrypted-media;'
+            width='560'
+            height='315'
             allowFullScreen
           >
-            <div className="themed-footer-text">
+            <div className='themed-footer-text'>
               Your browser does not support iFrames.
               <a
-                href="https://www.youtube.com/watch?v=P1yVieUmLPo&ab_channel=JonRumsey"
-                target="_blank"
-                rel="noreferrer"
+                href='https://www.youtube.com/watch?v=P1yVieUmLPo&ab_channel=JonRumsey'
+                target='_blank'
+                rel='noreferrer'
               >
                 View it at Jon&apos;s YouTube channel instead.
               </a>
@@ -86,10 +86,10 @@ export default function Description() {
           </iframe>
         </div>
       </Row>
-      <Row className="p-2 mb-3">
-        <Col md="auto"></Col>
-        <Col className="md-8">
-          <div className="rounded-4 themed-text" data-theme={theme}>
+      <Row className='p-2 mb-3'>
+        <Col md='auto'></Col>
+        <Col className='md-8'>
+          <div className='rounded-4 themed-text' data-theme={theme}>
             <ul>
               <li>
                 Whenever you hear or see a word on your bingoboard, just click
@@ -108,7 +108,7 @@ export default function Description() {
             </ul>
           </div>
         </Col>
-        <Col md="auto"></Col>
+        <Col md='auto'></Col>
       </Row>
       <Row>
         <Footer dataTheme={theme} />
