a1ef160ad361a4e8b7de0afd0b65dafc2fb315f4
Jon Rumsey <Jon.Rumsey@gmail.com>
2023-03-25 17:22:09 -0700

    implemented auth wrapper.

--- src/index.js
+++ src/index.js
@@ -7,23 +7,27 @@ import GameSession from './Components/GameSession.js';
 import About from './Components/About.js';
 import Description from './Components/Description.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import AuthProviderWrapper from './Auth/AuthProviderWrapper';
+import LoginButton from './Auth/LoginButton';
+import PresenterPage from './Components/PresenterPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter className='top-level-container'>
-      <Routes>
-        <Route path="/" element={<Home/>}>
-          <Route index element={<Description/>} />
-          <Route path="about" element = {<About/>}/>
-          <Route path="signup" element = {''}/>
-          <Route path="login" element = {''}/>
-          <Route path="play" element = {<GameSession/>}/>
-          <Route path="play/:gameboardId" element = {<GameSession/>}/>
-          <Route path="play/:gameboardId/:param2" element = {<GameSession/>}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <AuthProviderWrapper>
+      <BrowserRouter className='top-level-container'>
+        <Routes>
+          <Route path='/' element={<Home />}>
+            <Route index element={<Description />} />
+            <Route path='about' element={<About />} />
+            <Route path='presenter' element={<PresenterPage />} />
+            <Route path='login' element={<LoginButton />} />
+            <Route path='play' element={<GameSession />} />
+            <Route path='play/:gameboardId' element={<GameSession />} />
+            <Route path='play/:gameboardId/:param2' element={<GameSession />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProviderWrapper>
   </React.StrictMode>
 );
