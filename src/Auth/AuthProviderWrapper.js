2cdb31421983ba411d56176e406dff6dbf13c07b
Jon Rumsey <Jon.Rumsey@gmail.com>
2023-04-14 17:34:41 -0700

    commented out audience, scope

--- src/Auth/AuthProviderWrapper.js
+++ src/Auth/AuthProviderWrapper.js
@@ -4,12 +4,21 @@ import { Auth0Provider } from '@auth0/auth0-react';
 export default function AuthProviderWrapper({ children }) {
   const domain = process.env.REACT_APP_AUTH0_DOMAIN;
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+  // const audienceUrl = process.env.REACT_APP_AUDIENCEURL;
+  // const apiServer = process.env.REACT_APP_API_SERVER;
+  // const audience = `${apiServer}${audienceUrl}`;
+  // console.log('concatenated audience:', audience);
+  // const scope = process.env.REACT_APP_SCOPE;
 
   return (
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      authorizationParams={{ redirect_uri: window.location.origin }}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        // audience: audience,
+        // scope: scope,
+      }}
     >
       {children}
     </Auth0Provider>
