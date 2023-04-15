76e2e880e2cd250d1b7ac3ab11c2858fb6e3330f
Jon Rumsey <Jon.Rumsey@gmail.com>
2023-04-15 10:43:43 -0700

    uplift authn, z. use props to profile, forms

--- src/Components/PresenterPage.js
+++ src/Components/PresenterPage.js
@@ -1,17 +1,82 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card, Col, Container, Row } from 'react-bootstrap';
 import { useOutletContext } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
+import { Buffer } from 'buffer/';
+import axios from 'axios';
+import Footer from './Footer.js';
 import LoginButton from '../Auth/LoginButton';
 import LogoutButton from '../Auth/LogoutButton.js';
 import Profile from '../Auth/Profile.js';
-import Footer from './Footer.js';
 import PresenterForm from './PresenterForm';
-// import PresenterForm from './NoPresenterForm';
 
 export default function PresenterPage() {
   const [theme] = useOutletContext();
-  const { isAuthenticated, isLoading } = useAuth0();
+  const { isAuthenticated, isLoading, getAccessTokenSilently, user } =
+    useAuth0();
+  const [isApiAuthorized, setIsApiAuthorized] = useState(false);
+  const [encodedPayload, setEncodedPayload] = useState('');
+
+  useEffect(() => {
+    async function fetchData() {
+      if (user !== undefined) {
+        const isVerified = user.email_verified ? 'true' : 'false';
+        console.log('profile: user is defined and isVerified is', isVerified);
+        const accessToken = await getAccessTokenSilently({
+          authorizationParams: {
+            audience: process.env.REACT_APP_API_SERVER,
+            scope: 'read:current_user',
+          },
+        });
+        console.log('got access token', accessToken);
+        const payloadData = `${user.nickname},${user.email},${user.name},${isVerified}`;
+        const base64encodedPayload = Buffer.from(payloadData, 'utf8')
+          .toString('base64')
+          .replaceAll('+', '-')
+          .replaceAll('/', '_'); // convert base64 to base64url
+        setEncodedPayload(base64encodedPayload);
+        console.log('encodedPayload', encodedPayload);
+
+        const authorizeUrl = `${process.env.REACT_APP_AUDIENCEURL}/authorize/${encodedPayload}`;
+        const config = {
+          method: 'get',
+          baseURL: process.env.REACT_APP_API_SERVER,
+          url: authorizeUrl,
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        };
+        axios(config)
+          .then(() => setIsApiAuthorized(true))
+          .catch((error) => {
+            console.error(error);
+            setIsApiAuthorized(false);
+          });
+      } else {
+        setIsApiAuthorized(false);
+      }
+    }
+    fetchData();
+  }, [getAccessTokenSilently, setEncodedPayload, setIsApiAuthorized, user]);
+
+  if (isLoading) {
+    return (
+      <Card data-theme={theme} className='themed-background mt-4'>
+        <Card.Header data-theme={theme}>
+          <Card.Text data-theme={theme}>Loading. . .</Card.Text>
+        </Card.Header>
+      </Card>
+    );
+  }
+
+  if (isAuthenticated) {
+    console.log('Auth0 user object', user);
+    console.log('name, email, locale', user.name, user.email, user.locale);
+  }
+
+  if (isApiAuthorized) {
+    console.log('isApiAuthorized', isApiAuthorized);
+  }
 
   if (isLoading) {
     return (
@@ -36,17 +101,22 @@ export default function PresenterPage() {
             Presenters Page
           </div>
         </Col>
-        <Col></Col>
+        <Col>{isAuthenticated ? <LogoutButton /> : <LoginButton />}</Col>
       </Row>
       <Row>
-        <Col>{isAuthenticated && <Profile />}</Col>
-        <Col sm={4}>{!isAuthenticated && <LoginButton />}</Col>
-        <Col>{isAuthenticated && <LogoutButton />}</Col>
+        <Col>
+          {isAuthenticated && (
+            <Profile
+              username={user.name}
+              emailVerified={user.email_verified}
+              isApiAuthorised={isApiAuthorized}
+            />
+          )}
+        </Col>
       </Row>
       <Row>
         <Col className='md-8 pb-4'>
-          {/* {isAuthenticated && <PresenterForm />} */}
-          <PresenterForm />
+          <PresenterForm encodedPayload={encodedPayload} />
         </Col>
       </Row>
       <Row>
